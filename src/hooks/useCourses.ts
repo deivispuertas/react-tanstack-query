3f17b2131d0d30a08cca35c1ffae6b7b448d127f
devpuertas <deivispuertas0@gmail.com>
2025-05-09 16:10:17 -0500

    feat: add courses data and custom hook for fetching courses

---
+++ src/hooks/useCourses.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import type { QueryFunction } from "@tanstack/react-query";
+
+interface Course {
+    id: number;
+    title: string;
+    description: string;
+    duration: string;
+}
+
+const fetchCourses: QueryFunction<Course[]> = async () => {
+    const response = await fetch("api/course.json");
+    if(!response.ok){
+        throw new Error("Network response was not ok");
+    }
+    return response.json()
+}
+
+export const useCourses = () => {
+    return useQuery<Course[], Error>({
+        queryKey: ["courses"],
+        queryFn: fetchCourses,
+    });
+};
